53288e201bd91ca8cf281b6a4b4bb7596ed7811a
romangatiatullin <mr.artemka02@mail.ru>
2024-09-12 20:28:05 +0300

    Add files via upload

---
+++ module_2_2.py
@@ -0,0 +1,9 @@
+first = int(input('Введите целое число: '))
+second = int(input('Введите целое число: '))
+third = int(input('Ведите целое число: '))
+if first == second and third:
+    print(3)
+elif first == second or second == third or first == third:
+    print(2)
+else:
+    print(0)

aac79217dcb52a3b78a8ebd55449573266528ef3
romangatiatullin <mr.artemka02@mail.ru>
2024-09-13 22:26:28 +0300

    Update module_2_2.py

--- module_2_2.py
+++ module_2_2.py
@@ -1,7 +1,7 @@
 first = int(input('Введите целое число: '))
 second = int(input('Введите целое число: '))
 third = int(input('Ведите целое число: '))
-if first == second and third:
+if first == second and first == third:
     print(3)
 elif first == second or second == third or first == third:
     print(2)
